d7636cff14b604103a4498cfc4f57b72345bd0ed
leny <info@flatland.be>
2017-02-09 16:26:58 +0100

    Gulp task for scss

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@
  */
 
 var gulp = require( "gulp" ),
-    image = require( "gulp-image" );
+    image = require( "gulp-image" ),
+    sass = require( "gulp-sass" ),
+    autoprefixer = require( "gulp-autoprefixer" ),
+    csso = require( "gulp-csso" );
 
 // --- Task for images
 
@@ -18,6 +21,14 @@ gulp.task( "images", function() {
 } );
 
 // --- Task for styles
+
+gulp.task( "css", function() {
+    gulp.src( "src/sass/**/*.scss" )
+        .pipe( sass().on( "error", sass.logError ) )
+        .pipe( autoprefixer() )
+        .pipe( csso() )
+        .pipe( gulp.dest( "assets/css" ) );
+} );
 
 // --- Task for pug
 
