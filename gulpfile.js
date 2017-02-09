5dcd598a527be1a9201e0450a4e8a6280608e4ac
leny <info@flatland.be>
2017-02-09 15:42:57 +0100

    Setup gulp env

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/* leny/katbiznis
+ *
+ * /gulpfile.js - Gulp tasks
+ *
+ * coded by leny@flatLand!
+ * started at 09/02/2017
+ */
+
+var gulp = require( "gulp" );
+
+// --- Task for images
+
+// --- Task for styles
+
+// --- Task for pug
+
+// --- Task for js
+
+// --- Watch tasks
+
+// --- Aliases
