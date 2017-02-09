c987cdc3d754701d01b95a20f285aaed9311b651
leny <info@flatland.be>
2017-02-09 16:06:45 +0100

    Gulp task for images

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,16 @@
  * started at 09/02/2017
  */
 
-var gulp = require( "gulp" );
+var gulp = require( "gulp" ),
+    image = require( "gulp-image" );
 
 // --- Task for images
+
+gulp.task( "images", function() {
+    gulp.src( "src/images/**" )
+        .pipe( image() )
+        .pipe( gulp.dest( "assets/images" ) );
+} );
 
 // --- Task for styles
 
