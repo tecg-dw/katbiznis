781ebce11ca98aeb0bb804d0848f3bbd3f90f5c7
leny <info@flatland.be>
2017-02-23 10:57:55 +0100

    Add flipping code & styles

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,9 @@
  * started at 09/02/2017
  */
 
-console.log( "Hello, world!" );
+const $main = document.querySelector( "main" );
+
+document.querySelector( "nav a" ).addEventListener( "click", function( oEvent ) {
+    oEvent.preventDefault();
+    $main.classList.toggle( "flipped" );
+} );
